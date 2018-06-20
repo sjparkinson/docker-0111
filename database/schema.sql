9956acecb17302a98a2926e9db9f12b3aba84d35
Samuel Parkinson <sjparkinson@users.noreply.github.com>
2018-06-20 11:36:28 +0100

    More dogs.

--- database/schema.sql
+++ database/schema.sql
@@ -2,9 +2,26 @@ CREATE TABLE doggos (
   name varchar(255)
 );
 
-INSERT INTO doggos VALUES ('Floof Missile');
+INSERT INTO doggos VALUES ('Harley');
 INSERT INTO doggos VALUES ('Toofers');
+INSERT INTO doggos VALUES ('Lucky');
+INSERT INTO doggos VALUES ('Angel');
 INSERT INTO doggos VALUES ('Pupperina');
 INSERT INTO doggos VALUES ('Photogenic Doggo');
+INSERT INTO doggos VALUES ('Maggie');
+INSERT INTO doggos VALUES ('Daisy');
+INSERT INTO doggos VALUES ('Chloe');
+INSERT INTO doggos VALUES ('Buster');
+INSERT INTO doggos VALUES ('Bailey');
+INSERT INTO doggos VALUES ('Bella');
+INSERT INTO doggos VALUES ('Charlie');
+INSERT INTO doggos VALUES ('Murphy');
 INSERT INTO doggos VALUES ('Rare Pupper');
-INSERT INTO doggos VALUES ('Woofle');
+INSERT INTO doggos VALUES ('Molly');
+INSERT INTO doggos VALUES ('Floof Missile');
+INSERT INTO doggos VALUES ('Lucy');
+INSERT INTO doggos VALUES ('Ruby');
+INSERT INTO doggos VALUES ('Coco');
+INSERT INTO doggos VALUES ('Riley');
+INSERT INTO doggos VALUES ('Bear');
+INSERT INTO doggos VALUES ('Buddy');
